eb2d6804ceb33423d674e6fee67db54b642355a1
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-07-16 16:29:14 +0400

    Add `main` app URLs as endpoints

--- core/urls.py
+++ core/urls.py
@@ -16,14 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
-
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', include('main.urls')),
 
     path('advanced_filters/', include('advanced_filters.urls'))
 ]
